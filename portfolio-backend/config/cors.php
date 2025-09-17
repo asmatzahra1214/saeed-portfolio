d930b5241a967247bfe61d7b382003286a5ae09e
asmatzahra1214 <asmatzahranaqvi1214@gmail.com>
2025-09-16 22:51:15 -0700

    Stop tracking portfolio-backend folder

--- portfolio-backend/config/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// config/cors.php
-return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // Your React port
-    'allowed_headers' => ['*'],
-];
